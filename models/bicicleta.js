fd441bef0b015a17e2bf867c2489f0ecc81b4057
Eduardo Leon <eduleon78@gmail.com>
2022-06-26 09:14:23 -0400

    testing del modelo

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -31,10 +31,10 @@ Bicicleta.removeById = function(aBiciId) {
     }
 }
 
-var a = new Bicicleta(1, 'rojo', 'urbana', [10.989735, -63.828834]);
+/* var a = new Bicicleta(1, 'rojo', 'urbana', [10.989735, -63.828834]);
 var b = new Bicicleta(2, 'blanca', 'urbana', [10.983984, -63.827525]);
 
 Bicicleta.add(a);
-Bicicleta.add(b);
+Bicicleta.add(b); */
 
 module.exports = Bicicleta;
